3becaf485bf17b14e4d773ab31358f141238c853
linyimin-bupt <linyimin520812@gmail.com>
2018-12-18 14:12:40 +0800

    Add bit manipulation

---
+++ bit-manipulation/src/bit.ts
@@ -0,0 +1,62 @@
+class Bit {
+  static getBit(): number {
+    return 0
+  }
+
+  static setBit(): void {
+
+  }
+
+  static clearBit(): void {
+
+  }
+
+  static updateBit(number: number, bitPosition: number, bitValue: 0 | 1): void {
+
+  }
+
+  static isEven(number : number): boolean {
+    return false
+  }
+
+  static isPositive(number: number): boolean {
+    return false
+  }
+
+  static mutiplyByTwo(number: number): number {
+    return number << 1
+  }
+
+  static divideByTwo(number: number): number {
+    return number >> 1
+  }
+
+  static switchSign(number: number): number {
+    return 0
+  }
+
+  static multiply(a: number, b: number): number {
+    return 0
+  }
+
+  static multiplyUnsigned(a: number, b: number): number {
+    return 0
+  }
+
+  static countSetBit(number: number): number {
+    return 0
+  }
+
+  static bitsDiff(a: number, b: number): number {
+    return 0
+  }
+
+  static bitLength(number: number): number {
+    return 0
+  }
+
+  static isPowerOfTwo(number: number): boolean {
+    return false
+  }
+
+}
