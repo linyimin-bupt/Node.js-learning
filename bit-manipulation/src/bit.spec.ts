a8b8d4a906de1f66b84b61149e1a21d22cd60b84
linyimin-bupt <linyimin520812@gmail.com>
2018-12-21 21:21:02 +0800

    Add bit manipulation

--- bit-manipulation/src/bit.spec.ts
+++ bit-manipulation/src/bit.spec.ts
@@ -1,11 +1,311 @@
-import test                from 'blue-tape'
+import should from 'should'
 
 import { Bit } from './bit'
 
-test('setBit', (t) => {
-  const EXPECTED = 0x00110
-  const number   = 0x00100
-  const result   = Bit.setBit(number, 1)
+describe('Bit', () => {
 
-  t.equal(EXPECTED, result)
+  describe('#getBit', () => {
+    const number   = 0b00100
+    it('should return 0', () => {
+      const EXPECTED = 0
+      const result   = Bit.getBit(number, 0)
+      should(result).be.equal(EXPECTED)
+    })
+    it('should return 0', () => {
+      const EXPECTED = 0
+      const result   = Bit.getBit(number, 1)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 1', () => {
+      const EXPECTED = 1
+      const result   = Bit.getBit(number, 2)
+      should(result).be.equal(EXPECTED)
+    })
+    it('should return 0', () => {
+      const EXPECTED = 0
+      const result   = Bit.getBit(number, 3)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 0', () => {
+      const EXPECTED = 0
+      const result   = Bit.getBit(number, 4)
+      should(result).be.equal(EXPECTED)
+    })
+  })
+  
+  describe('#setBit', () => {
+    it('should return 0b0101', () => {
+      const EXPECTED = 0b0101
+      const number   = 0b0100
+      const result   = Bit.setBit(number, 0)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 0b0110', () => {
+      const EXPECTED = 0b0110
+      const number   = 0b0100
+      const result   = Bit.setBit(number, 1)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 0b0100', () => {
+      const EXPECTED = 0b0100
+      const number   = 0b0100
+      const result   = Bit.setBit(number, 2)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 0b1100', () => {
+      const EXPECTED = 0b1100
+      const number   = 0b0100
+      const result   = Bit.setBit(number, 3)
+      should(result).be.equal(EXPECTED)
+    })
+  })
+
+  describe('#clearBit', () => {
+    it('should return 0b0100', () => {
+      const EXPECTED = 0b0100
+      const number   = 0b0100
+      const result   = Bit.clearBit(number, 0)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 0b0100', () => {
+      const EXPECTED = 0b0100
+      const number   = 0b0100
+      const result   = Bit.clearBit(number, 1)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 0b0000', () => {
+      const EXPECTED = 0b0000
+      const number   = 0b0000
+      const result   = Bit.clearBit(number, 2)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 0b0000', () => {
+      const EXPECTED = 0b0000
+      const number   = 0b0000
+      const result   = Bit.clearBit(number, 3)
+      should(result).be.equal(EXPECTED)
+    })
+  })
+
+  describe('#updateBit', () => {
+    it('should return 0b0101', () => {
+      const EXPECTED = 0b0101
+      const number   = 0b0100
+      const result   = Bit.updateBit(number, 0, 1)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 0b0110', () => {
+      const EXPECTED = 0b0110
+      const number   = 0b0100
+      const result   = Bit.updateBit(number, 1, 1)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 0b0000', () => {
+      const EXPECTED = 0b0000
+      const number   = 0b0000
+      const result   = Bit.updateBit(number, 2, 0)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 0b1100', () => {
+      const EXPECTED = 0b1100
+      const number   = 0b0100
+      const result   = Bit.updateBit(number, 3, 1)
+      should(result).be.equal(EXPECTED)
+    })
+  })
+
+  describe('#isEven', () => {
+    it('should return true', () => {
+      const EXPECTED = true
+      const number   = 0b0100
+      const result   = Bit.isEven(number)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return false', () => {
+      const EXPECTED = false
+      const number   = 0b0101
+      const result   = Bit.isEven(number)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return false', () => {
+      const EXPECTED = false
+      const number   = 0b1111
+      const result   = Bit.isEven(number)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return true', () => {
+      const EXPECTED = true
+      const number   = 0b0000
+      const result   = Bit.isEven(number)
+      should(result).be.equal(EXPECTED)
+    })
+  })
+
+  describe('#isPositive', () => {
+    it('should return true', () => {
+      const EXPECTED = true
+      const number   = 0b0100
+      const result   = Bit.isPositive(number)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return false', () => {
+      const EXPECTED = false
+      const number   = 0b1101
+      const result   = Bit.isPositive(number, 4)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return false', () => {
+      const EXPECTED = false
+      const number   = 0b1111
+      const result   = Bit.isPositive(number, 4)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return false', () => {
+      const EXPECTED = false
+      const number   = 0b0000
+      const result   = Bit.isPositive(number)
+      should(result).be.equal(EXPECTED)
+    })
+  })
+
+  describe('#multiplyByTwo', () => {
+    it('should return 196', () => {
+      const EXPECTED = 196
+      const number   = 98
+      const result   = Bit.multiplyByTwo(number)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 2', () => {
+      const EXPECTED = 2
+      const number   = 1
+      const result   = Bit.multiplyByTwo(number)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 6', () => {
+      const EXPECTED = 6
+      const number   = 3
+      const result   = Bit.multiplyByTwo(number)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 100', () => {
+      const EXPECTED = 100
+      const number   = 50
+      const result   = Bit.multiplyByTwo(number)
+      should(result).be.equal(EXPECTED)
+    })
+  })
+
+  describe('#divideByTwo', () => {
+    it('should return 49', () => {
+      const EXPECTED = 49
+      const number   = 98
+      const result   = Bit.divideByTwo(number)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 0', () => {
+      const EXPECTED = 0
+      const number   = 1
+      const result   = Bit.divideByTwo(number)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 1', () => {
+      const EXPECTED = 1
+      const number   = 3
+      const result   = Bit.divideByTwo(number)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 25', () => {
+      const EXPECTED = 25
+      const number   = 50
+      const result   = Bit.divideByTwo(number)
+      should(result).be.equal(EXPECTED)
+    })
+  })
+
+  describe('#switchSign', () => {
+    it('should return 49', () => {
+      const EXPECTED = 49
+      const number   = -49
+      const result   = Bit.switchSign(number)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 0', () => {
+      const EXPECTED = 0
+      const number   = 0
+      const result   = Bit.switchSign(number)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 1', () => {
+      const EXPECTED = 1
+      const number   = -1
+      const result   = Bit.switchSign(number)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return -25', () => {
+      const EXPECTED = -25
+      const number   = 25
+      const result   = Bit.switchSign(number)
+      should(result).be.equal(EXPECTED)
+    })
+  })
+
+  describe('#multiply', () => {
+    it('should return 49', () => {
+      const EXPECTED = 49
+      const a   = 7
+      const b   = 7
+      const result   = Bit.multiply(a, b)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return -56', () => {
+      const EXPECTED = -56
+      const a   = 7
+      const b   = -8
+      const result   = Bit.multiply(a, b)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return -15', () => {
+      const EXPECTED = -15
+      const a   = 3
+      const b   = -5
+      const result   = Bit.multiply(a, b)
+      should(result).be.equal(EXPECTED)
+    })
+
+    it('should return 120', () => {
+      const EXPECTED = 120
+      const a   = -3
+      const b   = -40
+      const result   = Bit.multiply(a, b)
+      should(result).be.equal(EXPECTED)
+    })
+  })
 })
--- bit-manipulation/src/bit.ts
+++ bit-manipulation/src/bit.ts
@@ -7,23 +7,29 @@ export class Bit {
     return (1 << position) | number
   }
 
-  static clearBit(): void {
-
+  static clearBit(number: number, position: number): number {
+    return ~(1 << position) & number
   }
 
-  static updateBit(number: number, bitPosition: number, bitValue: 0 | 1): void {
-
+  static updateBit(number: number, position: number, bitValue: 0 | 1): number {
+    const clearMask = ~ (1 << position)
+    // Clear bit value and then set it up to required value
+    return clearMask & number | (bitValue  << position)
   }
 
   static isEven(number : number): boolean {
-    return false
+    return (number & 1) === 0
   }
 
-  static isPositive(number: number): boolean {
-    return false
+  static isPositive(number: number, bitNum?: number): boolean {
+    if (number === 0) {
+      return false
+    }
+    bitNum = bitNum ? bitNum - 1 : 31
+    return ((number >> bitNum) & 1) === 0
   }
 
-  static mutiplyByTwo(number: number): number {
+  static multiplyByTwo(number: number): number {
     return number << 1
   }
 
@@ -32,31 +38,41 @@ export class Bit {
   }
 
   static switchSign(number: number): number {
-    return 0
+    if (number < 0) {
+      return ~(number - 1)
+    }
+    return ~number + 1 
   }
 
   static multiply(a: number, b: number): number {
-    return 0
+    if (a === 0 || b === 0) {
+      return 0
+    }
+    const multiplyByOdd = () => this.multiply(this.multiplyByTwo(a), this.divideByTwo(b + 1)) - a
+
+    const multiplyByEven = () => this.multiply(this.multiplyByTwo(a), this.divideByTwo(b))
+
+    return this.isEven(b) ? multiplyByEven() : multiplyByOdd()
   }
 
   static multiplyUnsigned(a: number, b: number): number {
     return 0
   }
 
-  static countSetBit(number: number): number {
-    return 0
-  }
+  // static countSetBit(number: number): number {
+  //   return 0
+  // }
 
-  static bitsDiff(a: number, b: number): number {
-    return 0
-  }
+  // static bitsDiff(a: number, b: number): number {
+  //   return 0
+  // }
 
-  static bitLength(number: number): number {
-    return 0
-  }
+  // static bitLength(number: number): number {
+  //   return 0
+  // }
 
-  static isPowerOfTwo(number: number): boolean {
-    return false
-  }
+  // static isPowerOfTwo(number: number): boolean {
+  //   return false
+  // }
 
 }
